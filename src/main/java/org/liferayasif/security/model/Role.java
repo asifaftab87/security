03cad081a01d78b4dfb62931841992caf4e8c8e3
Mohammad Atif Aftab <iamatif96@gmail.com>
2020-03-26 19:03:14 +0800

    Adding getters n setters to avoid error

--- src/main/java/org/liferayasif/security/model/Role.java
+++ src/main/java/org/liferayasif/security/model/Role.java
@@ -20,4 +20,26 @@ public class Role {
     private int id;
     @Column(name = "role")
     private String role;
+    
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+    
+    @Override
+    public String toString() {
+    	return "roleid: "+id+"    role: "+role;
+    }
+    
 }
